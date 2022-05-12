d0dd36594d84295156f8f1ba46b6aebeff04fb9f
SHIVANK VARSHNEY <74199623+Shivank-Varshney@users.noreply.github.com>
2022-05-12 12:03:03 +0530

    todo app ready to use

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Img from "./list.png";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState("");
+  const [list, setList] = useState(["Dummy data #1", "Dummy data #2"]);
+
+  const SubmitEvent = (e) => {
+    e.preventDefault();
+    setList([data, ...list]);
+    setData("");
+  };
+
+  const removeListData = (id) => {
+    const updatedList = list.filter((val, index) => {
+      return id !== index;
+    });
+    setList(updatedList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="todoList">
+        <img src={Img} alt="list" className="img-fluid listImg" />
+        <div className="listTitle">Add Your Task Here...&#129312; </div>
+        <form onSubmit={SubmitEvent}>
+          <div className="inputArea">
+            <input
+              type="text"
+              value={data}
+              onChange={(e) => setData(e.target.value)}
+              className="form-control"
+              placeholder="Add your task here"
+              required
+            />
+            <button type="submit">
+              <i class="fa-solid fa-plus"></i>
+            </button>
+          </div>
+        </form>
+
+        {/* ToDo list */}
+        {list.map((val, index) => {
+          return (
+            <div className="listItem" key={index}>
+              <span> {val}</span>
+              <i
+                class="fa-solid fa-trash-can"
+                onClick={() => removeListData(index)}
+              ></i>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
-}
+};
 
 export default App;
